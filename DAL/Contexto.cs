1a718b57a26c686205d5e2454d68048227773dab
ralfy <ralfy@RALFY>
2024-11-18 01:51:18 -0300

    agregando la carpeta DAL y creando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RalfyGenao_AP1_P2.Models;
+using System.Collections.Generic;
+
+namespace RalfyGenao_AP1_P2.DAL
+{
+    public class Contexto :DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Registros> registros { get; set; }
+    }
+}
