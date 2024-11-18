97fe05d9af718fb6c14fb2e974f8277f612ead4a
ralfy <ralfy@RALFY>
2024-11-18 01:51:42 -0300

    Agregando la carpeta Models y creando el modelo

---
+++ Models/Registros.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RalfyGenao_AP1_P2.Models
+{
+    public class Registros
+    {
+        [Key]
+        public int RegistroId { get; set; }
+        [Required(ErrorMessage = "Por Favor Completar el Campos")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "Por Favor Completar el Campos")]
+        public string? Descripcion { get; set; }
+    }
+}
